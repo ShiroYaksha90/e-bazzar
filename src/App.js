c22bc09e22e228a2867f390f0fe122ae4791c9f8
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-10-28 21:11:09 +0300

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-const App= ()=> {
-  return (
-    <>
-      <Header/>
-        <h1>Welcome to e-bazzar</h1>
-      <Footer/>
-    </>
-  );
-}
+
+const App = () => (
+  <>
+    <Header />
+    <h1>Welcome to e-bazzar</h1>
+    <Footer />
+  </>
+);
 
 export default App;
