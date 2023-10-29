8ed405be4c937605a26187f6c940e36f5c041235
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-10-29 11:24:48 +0300

    Import & add container component from bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 const App = () => (
   <>
     <Header />
-    <h1>Welcome to e-bazzar</h1>
+    <main className="py-3">
+      <Container>
+        <h1>Welcome to e-bazzar</h1>
+      </Container>
+    </main>
     <Footer />
   </>
 );
