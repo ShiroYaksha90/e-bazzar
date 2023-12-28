bea8c253961b356370d01b56473d3e5afbd9e516
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-12-28 17:46:01 +0300

    Add Loader and Message components to HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import listProducts from '../actions/productActions';
+import Loader from '../components/loader';
+import Message from '../components/message';
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
@@ -16,9 +18,11 @@ const HomeScreen = () => {
     <>
       <h1>Latest Products</h1>
       {loading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant="danger">
+          {error}
+        </Message>
       ) : (
         <Row>
           {products.map((product) => (
