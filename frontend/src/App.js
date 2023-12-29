a502f3571931cbc9641009563fc5a307b461da12
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-12-29 09:59:11 +0300

    Add CartScreen component to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/cartScreen';
 
 const App = () => (
   <Router>
@@ -14,6 +15,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<HomeScreen />} exact />
           <Route path="/product/:id" element={<ProductScreen />} />
+          <Route path="/cart/:id?" element={<CartScreen />} />
         </Routes>
       </Container>
     </main>
